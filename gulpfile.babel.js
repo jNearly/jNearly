6aea79dd490a869441497dd9e92f67e41a54751f
Callum Macrae <callum@macr.ae>
2016-01-21 10:00:21 +0000

    Split jNearly.com into new repo. Slightly broken.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,43 +1,39 @@
 import gulp from 'gulp';
 import sourcemaps from 'gulp-sourcemaps';
 
+import replace from 'gulp-replace';
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
-
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import postcssImport from 'postcss-import';
-
-import generateDocsPlugin from './gulp/generateDocs';
 
 function js() {
 	return gulp.src(['src/index.js', 'src/**/*.js'])
 		.pipe(sourcemaps.init())
-		.pipe(babel())
-		.pipe(concat('bundle.js'))
+		.pipe(concat('jnearly.js'))
+		//.pipe(replace('function $(', 'export default function $('))
+		.pipe(babel({
+		//	plugins: ['transform-es2015-modules-umd'],
+			moduleId: '$'
+		}))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
 }
 
-function css() {
-	return gulp.src('app/assets/css/styles.css')
-		.pipe(postcss([ autoprefixer(['last 2 versions', '> 5%']), postcssImport ]))
-		.pipe(gulp.dest('app/assets/build'));
-}
-
-function generateDocs() {
-	return gulp.src('src/**/*.js')
-		.pipe(generateDocsPlugin())
-		.pipe(gulp.dest('app'));
-}
-
 gulp.task(js);
-gulp.task(generateDocs);
-
-gulp.task('default', gulp.parallel(js, css, generateDocs));
+gulp.task('default', js);
 
 if (process.argv.indexOf('--watch') !== -1) {
 	gulp.watch('./src/**/*.js', js);
-	gulp.watch('./app/assets/css/**/*.css', css);
-	gulp.watch(['./src/**/*.js', './templates/*.html'], generateDocs);
+
+	var browserSync = require('browser-sync').create();
+
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+		startPath: '/test/runner.html'
+	});
+
+	gulp.watch(['dist/*.js', 'test/runner.html', 'test/**/*.js'], function () {
+		browserSync.reload();
+	});
 }
