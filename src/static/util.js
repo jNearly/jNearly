1882ec07264d9059ef07215b1d797814ae6b47b5
Callum Macrae <callum@macr.ae>
2016-01-21 09:59:35 +0000

    Added more util functions

--- src/static/util.js
+++ src/static/util.js
@@ -5,3 +5,144 @@
 $.unique = function (arr) {
 	return arr.filter((val, i) => arr.indexOf(val) === i);
 };
+
+/**
+ * To merge objects together, we can use `Object.assign()` for shallow copies.
+ * For deep copies, however, we use an iterator to iterate through, calling
+ * `Object.assign()` when we can.
+ */
+$.extend = function (...objects) {
+	if (objects[0] !== true) {
+		// We can use Object.assign() if it's not a deep copy
+		return Object.assign(...objects);
+	}
+
+	let returnObject = objects[1];
+
+	for (let object of objects.slice(2)) {
+		mergeOntoFirst(returnObject, object);
+	}
+
+	return returnObject;
+
+	// Destructively deep copies properties from the second object to the first
+	function mergeOntoFirst(first, second) {
+		for (let key of Object.keys(second)) {
+			if (typeof first[key] === 'object' && typeof second[key] === 'object') {
+				mergeOntoFirst(first[key], second[key]);
+			} else {
+				first[key] = second[key];
+			}
+		}
+	}
+};
+
+/**
+ * We use the spread operator to push every elements of the second array onto
+ * the end of the first array. We can't use concat, because that's not
+ * destructive.
+ */
+$.merge = function (ary1, ary2) {
+	if (!Array.isArray(ary1)) {
+		ary1 = Array.from(ary1);
+	}
+
+	ary1.push(...ary2);
+	return ary1;
+};
+
+/**
+ * I'm not sure why jQuery felt the need to replicate the `.indexOf()`
+ * functionality in arrays. You can just use `.indexOf()` directly.
+ */
+$.inArray = function (value, array, fromIndex = 0) {
+	return array.indexOf(value, fromIndex);
+};
+
+/**
+ * To check for an empty object, we attempt to iterate through it. This is
+ * actually the same code jQuery uses to do it.
+ */
+$.isEmptyObject = function (obj) {
+	for (var name in obj) {
+		return false;
+	}
+
+	return true;
+};
+
+/**
+ * These type checking functions are both trivial in JavaScript.
+ */
+$.isFunction = (obj) => typeof obj === 'function';
+$.isWindow = (obj) => obj != null && obj.window === obj;
+
+/**
+ * To see if a value is numeric, we attempt to subtract it from itself and
+ * compare it to zero. We have two special cases here: an array subtracted from
+ * itself is zero, and `null - null` is also zero. To get around this, we call
+ * `parseFloat` on one of them, which will return `NaN` for values like `null`.
+ *
+ * Why aren't we just using `typeof` and then checking for null? Well, that
+ * would return false for strings containing numbers: `$.isNumeric("2")` should
+ * return true.
+ */
+$.isNumeric = function (obj) {
+	// Special case: `[1] - parseFloat([1])` evaluates to 0
+	if (Array.isArray(obj)) {
+		return false;
+	}
+
+	return Math.abs(obj - parseFloat(obj)) + Number.EPSILON > 0;
+};
+
+/**
+ * These functions all now exist in JavaScript: no need to do anything other
+ * than alias them.
+ */
+$.parseJSON = JSON.parse;
+$.isArray = Array.isArray;
+$.makeArray = (obj) => Array.from(obj);
+$.now = Date.now;
+
+// This too exists in JavaScript, but is on the prototype
+$.trim = (str) => str.trim();
+
+/**
+ * $.type behaves similar to `typeof`, but with a bit less weirdness.
+ *
+ * First, we test for a few types that don't exist in JavaScript, and if it is
+ * one of them, we return that value.
+ *
+ * Then, we call the `.valueOf()` method of the argument and return the type of
+ * that. This is done because `typeof new String('hello')` is actually
+ * "object", not "string" as expected. Calling `.valueOf()` returns the string
+ * itself.
+ */
+$.type = function (obj) {
+	if (obj === undefined) {
+		return 'undefined';
+	}
+
+	if (obj === null) {
+		return 'null';
+	}
+
+	if (Array.isArray(obj)) {
+		return 'array';
+	}
+
+	if (obj instanceof Date) {
+		return 'date';
+	}
+
+	if (obj instanceof Error) {
+		return 'error';
+	}
+
+	if (obj instanceof RegExp) {
+		return 'regexp';
+	}
+
+	return typeof obj.valueOf();
+};
--- test/static/util.spec.js
+++ test/static/util.spec.js
@@ -16,4 +16,250 @@ describe('Util functions', function () {
 			$.unique(nodeArray).length.should.equal(3);
 		});
 	});
+
+	describe('$.extend()', function () {
+		it('should shallow copy simple objects', function () {
+			var obj = $.extend({ one: 1 }, { two: 2 });
+			obj.should.deepEqual({ one: 1, two: 2 });
+		});
+
+		it('should shallow copy nested objects', function () {
+			var obj = $.extend({ one: 1, foo: { three: 3 } }, { two: 2, foo: { four: 4 } });
+			obj.should.deepEqual({ one: 1, two: 2, foo: { four: 4 } });
+		});
+
+		it('should deep copy simple objects', function () {
+			var obj = $.extend(true, { one: 1 }, { two: 2 });
+			obj.should.deepEqual({ one: 1, two: 2 });
+		});
+
+		it('should deep copy nested objects', function () {
+			var obj = $.extend(true, { one: 1, foo: { three: 3 } }, { two: 2, foo: { four: 4 } });
+			obj.should.deepEqual({ one: 1, two: 2, foo: { three: 3, four: 4 } });
+		});
+
+		it('should deep copy super nested objects', function () {
+			var obj = $.extend(true, { one: 1, foo: { three: 3, bar: { five: 5, six: 6 } } }, { two: 2, foo: { four: 4, bar: { seven: 7 } } });
+			obj.should.deepEqual({ one: 1, two: 2, foo: { three: 3, four: 4, bar: { five: 5, six: 6, seven: 7 }}});
+		});
+	});
+
+	describe('$.merge()', function () {
+		it('should merge two arrays', function () {
+			$.merge([1, 2, 3], [4, 5, 6]).should.deepEqual([1, 2, 3, 4, 5, 6]);
+		});
+
+		it('should merge destructively', function () {
+			var one = [1, 2, 3];
+			$.merge(one, [4, 5, 6]);
+
+			one.should.deepEqual([1, 2, 3, 4, 5, 6]);
+		});
+
+		it('should merge array-like objects', function () {
+			var nodes = document.querySelectorAll('#looping p');
+			(function () {
+				$.merge(arguments, nodes);
+			})(1, 2, 3);
+		});
+	});
+
+	describe('$.inArray()', function () {
+		it('should return index if in the array', function () {
+			$.inArray('dog', ['cat', 'dog', 'bat']).should.equal(1);
+		});
+
+		it('should return index if in the array after specified index', function () {
+			$.inArray(1, [1, 2, 3, 4, 1, 2, 3, 4], 2).should.equal(4);
+		});
+
+		it('should return -1 if not in the array', function () {
+			$.inArray('1', [1, 2, 3]).should.equal(-1);
+		});
+
+		it('should return -1 if not after specified index', function () {
+			$.inArray(1, [1, 2, 3], 2).should.equal(-1);
+		});
+	});
+
+	describe('$.isEmptyObject()', function () {
+		it('should return true on empty objects', function () {
+			$.isEmptyObject({}).should.be.True();
+		});
+
+		it('should return false on simple objects', function () {
+			$.isEmptyObject({ one: 1 }).should.be.False();
+		});
+
+		it('should return false on complex objects', function () {
+			function Test(){}
+			Test.prototype.one = 1;
+			var obj = new Test();
+
+			$.isEmptyObject(obj).should.be.False();
+		});
+	});
+
+	describe('$.isFunction()', function () {
+		it('should return true for functions', function () {
+			$.isFunction($).should.be.True();
+		});
+
+		it('should return false for non-functions', function () {
+			$.isFunction({}).should.be.False();
+		});
+	});
+
+	describe('$.isWindow()', function () {
+		it('should return true for window', function () {
+			$.isWindow(window).should.be.True();
+		});
+
+		it('should return false when not', function () {
+			$.isWindow(document).should.be.False();
+		});
+
+		it('should return true for iframe windows', function () {
+			var iframe = $('#util iframe')[0];
+
+			$.isWindow(iframe.contentWindow).should.be.True();
+		});
+	});
+
+	describe('$.isNumeric()', function () {
+		it('should return true for simple numbers', function () {
+			$.isNumeric(4).should.be.True();
+		});
+
+		it('should return true for simple number strings', function () {
+			$.isNumeric('4').should.be.True();
+		});
+
+		it('should return false for arrays', function () {
+			$.isNumeric([1]).should.be.False();
+		});
+
+		it('should work for all the examples in jQuery docs', function () {
+			$.isNumeric( '-10' ).should.be.True();
+			$.isNumeric( 16 ).should.be.True();
+			$.isNumeric( 0xFF ).should.be.True();
+			$.isNumeric( '0xFF' ).should.be.True();
+			$.isNumeric( '8e5' ).should.be.True();
+			$.isNumeric( 3.1415 ).should.be.True();
+			$.isNumeric( +10 ).should.be.True();
+			$.isNumeric( '' ).should.be.False();
+			$.isNumeric({}).should.be.False();
+			$.isNumeric( NaN ).should.be.False();
+			$.isNumeric( null ).should.be.False();
+			$.isNumeric( true ).should.be.False();
+			$.isNumeric( Infinity ).should.be.False();
+			$.isNumeric( undefined ).should.be.False();
+		});
+	});
+
+	describe('$.parseJSON()', function () {
+		it('should parse JSON', function () {
+			$.parseJSON('{ "foo": "bar" }').should.deepEqual({ foo: 'bar' });
+			$.parseJSON('{ "foo": [ 0, true, "yes" ] }').should.deepEqual({ foo: [0, true, 'yes'] });
+		});
+
+		it('should throw an error on invalid JSON', function () {
+			(function () {
+				$.parseJSON('{ foo: "bar" }');
+			}).should.throw();
+
+			(function () {
+				$.parseJSON('{ "foo": function () {} }');
+			}).should.throw();
+		});
+	});
+
+	describe('$.isArray()', function () {
+		it('should return true when true', function () {
+			$.isArray([]).should.be.True();
+		});
+
+		it('should return false when false', function () {
+			$.isArray('').should.be.False();
+		});
+	});
+
+	describe('$.makeArray()', function () {
+		it('should make arrays from array-like objects', function () {
+			var nodes = document.querySelectorAll('#looping p');
+			var array = $.makeArray(nodes);
+
+			nodes.length.should.equal(array.length);
+			nodes[4].should.equal(array[4]);
+		});
+	});
+
+	describe('$.now()', function () {
+		it('should return some sort of number', function () {
+			$.now().should.be.type('number');
+			($.now() - Date.now()).should.be.within(0, 100);
+		});
+	});
+
+	describe('$.trim()', function () {
+		it('should trim', function () {
+			$.trim('\ta   b   c   ').should.equal('a   b   c');
+		});
+	});
+
+	describe('$.type()', function () {
+		it('should return undefined', function () {
+			$.type(undefined).should.equal('undefined');
+			$.type().should.equal('undefined');
+			$.type(window.notDefined).should.equal('undefined');
+		});
+
+		it('should return null', function () {
+			$.type(null).should.equal('null');
+		});
+
+		it('should return boolean', function () {
+			$.type(true).should.equal('boolean');
+			$.type(new Boolean()).should.equal('boolean');
+		});
+
+		it('should return number', function () {
+			$.type(3).should.equal('number');
+			$.type(new Number(3)).should.equal('number');
+		});
+
+		it('should return string', function () {
+			$.type('test').should.equal('string');
+			$.type(new String('test')).should.equal('string');
+		});
+
+		it('should return function', function () {
+			$.type(function(){}).should.equal('function');
+			$.type($).should.equal('function');
+		});
+
+		it('should return array', function () {
+			$.type([]).should.equal('array');
+			$.type(new Array()).should.equal('array');
+		});
+
+		it('should return date', function () {
+			$.type(new Date()).should.equal('date');
+		});
+
+		it('should return error', function () {
+			$.type(new Error()).should.equal('error');
+
+			try {
+				JSON.parse('{')
+			} catch (e) {
+				$.type(e).should.equal('error');
+			}
+		});
+
+		it('should return regexp', function () {
+			$.type(new RegExp('test')).should.equal('regexp');
+			$.type(/test/).should.equal('regexp');
+		});
+	});
 });
